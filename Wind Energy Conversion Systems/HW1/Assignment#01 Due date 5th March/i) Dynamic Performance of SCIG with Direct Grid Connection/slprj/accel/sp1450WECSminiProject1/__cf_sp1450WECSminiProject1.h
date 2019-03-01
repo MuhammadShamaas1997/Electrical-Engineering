3471cf4fe594b37213978061e42fe26d89c78311
Muhammad Shamaas <mshamaas1997@gmail.com>
2019-03-01 23:42:27 +0500

    V1

---
+++ Wind Energy Conversion Systems/HW1/Assignment#01 Due date 5th March/i) Dynamic Performance of SCIG with Direct Grid Connection/slprj/accel/sp1450WECSminiProject1/__cf_sp1450WECSminiProject1.h
@@ -0,0 +1,3 @@
+#ifndef CF_sp1450WECSminiProject1_H__
+#define CF_sp1450WECSminiProject1_H__
+#endif
---
+++ Wind Energy Conversion Systems/HW1/Assignment#01 Due date 5th March/i) Dynamic Performance of SCIG with Direct Grid Connection/slprj/accel/sp1450WECSminiProject1/sp1450WECSminiProject1_acc_types.h
@@ -0,0 +1,6 @@
+#include "__cf_sp1450WECSminiProject1.h"
+#ifndef RTW_HEADER_sp1450WECSminiProject1_acc_types_h_
+#define RTW_HEADER_sp1450WECSminiProject1_acc_types_h_
+#include "rtwtypes.h"
+typedef struct P_sp1450WECSminiProject1_T_ P_sp1450WECSminiProject1_T ;
+#endif
