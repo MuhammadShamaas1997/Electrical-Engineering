b1480bf5231193124c3057d8d85a5394ca50ccbe
Muhammad Shamaas <mshamaas1997@gmail.com>
2019-04-12 23:36:51 +0500

    V1

--- Wind Energy Conversion Systems/Assignment#4/grid_side_VOC_Constants.m
+++ Wind Energy Conversion Systems/Assignment#4/grid_side_VOC_Constants.m
@@ -1,7 +1,7 @@
 clc
 clear
 F0=1000;
-Grid_voltage_init = 0;
+Grid_Angle_init = pi;
 iq_ref_pu = 0;
 % rectifer or inverter selection
 % "1" --- rectifier
@@ -10,35 +10,37 @@ Rectifier_or_inverter=0;
 
 
 Pb=2.3e6;% active power at the PCC
-Vb=690*2^0.5/3^0.5;%563.3826
+Vp=(690*(2^0.5))/(3^0.5);%563.3826
+Vbase=Vp/(2^.5);
 Fb=60;
-Ib=Pb/(1.5*Vb);
-Zb=Vb/Ib;
+Ibase=(Pb/3)/(690/(3^0.5));
+Zb=Vbase/Ibase;
 Omega_b=2*pi*Fb;
-Lb=Zb/Omega_b;
-Cb=1/(Omega_b*Zb);
+XLb=Zb*Omega_b;
+XCb=1/(Omega_b*Zb);
 
 % grid-side filter inductor
-Lgrid_pu=0.2*Fb;
-Lgrid=Lgrid_pu*Lb;
-Rgrid_pu=0.005;
+Lgrid=0.1098e-3;
+XLgrid=0.2*XLb;
+XLgrid_pu=XLgrid/Zb;
+Rgrid_pu=0.1;
 Rgrid=Rgrid_pu*Zb;
 Power_loss=Rgrid_pu*Pb;
 
 %dc link
-Cdc=4*Cb;
-Vdc_ref=(Lgrid_pu+1.0+0.05)*3^0.5;% in per unit
-Vdc_capacitor_initial=Vdc_ref*Vb;
+Cdc=0.3*Zb;
+Vdc_ref=3.062;% in per unit
+Vdc_capacitor_initial=Vdc_ref*Vbase;
 
 %dc supply voltage calculation according to series resistor
 Rdc_series=0.1*Zb; % might be changed here
 
 if Rectifier_or_inverter==1 % for rectifier operation case
-    Idc=(Pb - Power_loss)/(Vdc_ref*Vb);
-    Vdc_supply=Vdc_ref*Vb - Idc*Rdc_series;
+    Idc=(Pb - Power_loss)/(Vdc_ref*Vbase);
+    Vdc_supply=Vdc_ref*Vbase - Idc*Rdc_series;
 elseif Rectifier_or_inverter==0% for inverter operation case
-    Idc=(Pb + Power_loss)/(Vdc_ref*Vb);
-    Vdc_supply=Vdc_ref*Vb + Idc*Rdc_series;
+    Idc=(Pb + Power_loss)/(Vdc_ref*Vbase);
+    Vdc_supply=Vdc_ref*Vbase + Idc*Rdc_series;
 end
 
 %switching frequency
