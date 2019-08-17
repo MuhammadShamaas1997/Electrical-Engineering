40ee14609a6ff4d3baca3c02df33a680831c2c24
Muhammad Shamaas <mshamaas1997@gmail.com>
2019-08-18 01:11:43 +0500

    V1

--- Wind Energy Conversion Systems/Assignment#3/synch2.m
+++ Wind Energy Conversion Systems/Assignment#3/synch2.m
@@ -8,9 +8,17 @@ T=1/f;
 Ld=9.816e-3;
 Lq=9.816e-3;
 Rs=24.21e-3;
-RL=6;
 %%dt=1e-4;              %step size (0.0001second)
 %%t=0;
 %for t=0:dt:1
 %update the values of time systematically to be used in sine & cosine(w*t)
 %end
+
+RL=4.2855;
+LL=8.258e-3;
+Rsnubber=1e4;
+iqs=((w*7.030056*(RL+Rs))/((RL+Rs)*(RL+Rs)+w*w*(LL+Ld)*(LL+Lq)));
+iqspu=iqs/(490*sqrt(2));
+ids=((w*(LL+Lq)*iqs)/(Rs+6));
+idspu=ids/(490*sqrt(2));
+is=(iqs*iqs+ids*ids)^0.5;
