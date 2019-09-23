f8889b506a00519058f556d754f1e2501542c462
Muhammad Shamaas <mshamaas1997@gmail.com>
2019-09-23 05:12:41 +0500

    V1

--- Electric Drives Control/Assignment1/Q4.m
+++ Electric Drives Control/Assignment1/Q4.m
@@ -1,11 +1,10 @@
-Tsw=200e-6;
-Udc=140;
-subplot(2,1,1)
-plot(ia.time,ia.data); grid on; hold on;
-stairs(ia_k.time,ia_k.data,'r'); % Discrete signal
-%axis([0.15 0.1504 9 11]); % Controls axis scaling
-xlabel('Time (s)'); ylabel('Current (A)');
-subplot(2,1,2)
-plot(ua.time,ua.data); grid on;
-%axis([0.15 0.1504 -10 150]);
-xlabel('Time (s)'); ylabel('Voltage (V)');
+subplot(2,1,1); % Divides the figure to two subplots
+%plot(ia.time,ia.data); grid on;
+IN = 20; % Rated current
+plot(ia.time,ia.data/IN); grid on;% This would plot the p.u. current
+xlabel('Time (s)'); ylabel('per unit Current (p.u.)');
+subplot(2,1,2);
+nN=3000;%rev/min
+wN=nN*(2*pi/60);
+plot(wM.time,wM.data/wN); grid on;
+xlabel('Time (s)'); ylabel('per unit Speed (p.u.)');
