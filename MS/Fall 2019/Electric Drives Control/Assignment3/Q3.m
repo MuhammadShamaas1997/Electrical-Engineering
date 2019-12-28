7273c50b8aca0e2af5f5b1bf12c07aebe3c871e7
Muhammad Shamaas <mshamaas1997@gmail.com>
2019-12-29 02:35:49 +0500

    V1

---
+++ MS/Fall 2019/Electric Drives Control/Assignment3/Q3.m
@@ -0,0 +1,11 @@
+subplot(3,1,1); % Divides the figure to two subplots
+plot(iqs.time,iqs.data); grid on;%axis([0 5 1.56 1.57]);% This would plot the current
+xlabel('Time (s)'); ylabel('iqs (A)');
+
+subplot(3,1,2);
+plot(ids.time,ids.data); grid on;%axis([0 5 7.81 7.82]);% This would plot the current
+xlabel('Time (s)'); ylabel('ids (A)');
+
+subplot(3,1,3);
+plot(wM.time,wM.data); grid on;
+xlabel('Time (s)'); ylabel('Speed (rad/s)');
---
+++ MS/Fall 2019/Electric Drives Control/Assignment3/Q7.m
@@ -0,0 +1,11 @@
+subplot(3,1,1); % Divides the figure to two subplots
+plot(iqs.time,iqs.data); grid on;%axis([0 5 1.56 1.57]);% This would plot the current
+xlabel('Time (s)'); ylabel('iqs (A)');
+
+subplot(3,1,2);
+plot(ids.time,ids.data); grid on;%axis([0 5 7 8]);% This would plot the current
+xlabel('Time (s)'); ylabel('ids (A)');
+
+subplot(3,1,3);
+plot(wr.time,wr.data); grid on;
+xlabel('Time (s)'); ylabel('Speed (rad/s)');
