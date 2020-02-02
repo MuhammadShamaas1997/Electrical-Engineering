8ef6138e4e831e5ca80dca7de0051619bdb23dec
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-02-03 02:35:11 +0500

    V1

---
+++ MS/Spring 2020/Thesis/Magnetic Transmission Lines/MEEP/Readings/FluxTransfer.m
@@ -0,0 +1,25 @@
+clc;clear all;
+f=fopen('FluxTransfer.txt');
+l=fgetl(f);
+i=1;
+while ischar(l)
+    %%disp(l);
+    text{i}=l;
+    data{i}=sscanf(text{i},'%f %f %f');
+    A1(i)=data{i}(1);
+    A2(i)=data{i}(2);
+    A3(i)=data{i}(3);
+    l=fgetl(f);
+    i=i+1;
+end
+
+hold on;
+
+subplot(3,1,1)
+A=abs(A2)/max(abs(A2));%Flux_in
+B=abs(A3)/max(abs(A2));%Flux_out
+plot(A1,A);
+subplot(3,1,2)
+plot(A1,B);
+subplot(3,1,3)
+plot(A1,B./A);
