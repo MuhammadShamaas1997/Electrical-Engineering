ee2618c261754dad94cb9a2455e10cac981d4ddc
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-03-01 23:47:43 +0500

    V1

--- MS/Spring 2020/Advanced Power System Operation and Control/Assignment 2/Assignment1.m
+++ MS/Spring 2020/Advanced Power System Operation and Control/Assignment 2/Assignment1.m
@@ -27,6 +27,7 @@ ordered_s=sort(transpose(reshape(s,[],1)));
 
 %%
 Pgen=[0 0 0;0 0 0];
+Pgen=[Pmin(1) Pmin(2) Pmin(3);Pmax(1) Pmax(2) Pmax(3)];
 eps1=Pload;
 eps2=Pload;
 threshold=1;
@@ -38,15 +39,10 @@ found=0;
 Ans=[];
 OldCostMin=1e5;
 Oldeps=Pload;
+lamdaOld=0;
 
 for n=1:maxiter
     lamda=ordered_s(n);
-    hold on
-    %     stairs(sPmin(1,:),s(1,:),'r')
-    %     stairs(sPmin(2,:),s(2,:),'b')
-    %     stairs(sPmin(3,:),s(3,:),'g')
-    plot([50,600],[lamda,lamda])
-    %plot(Pgen,lamda,'o')
     
     for rep=1:2
         for i=1:Ngen
@@ -60,6 +56,9 @@ for n=1:maxiter
                     else
                         Pgen(2,i)=sPmax(i,k);
                     end
+                    if (divisions==1)
+                        Pgen(1,i)=sPmax(i,k);
+                    end
                 end
                 
                 if (s(i,k)>lamda)
@@ -69,6 +68,9 @@ for n=1:maxiter
                         end
                     else
                         Pgen(1,i)=sPmin(i,k);
+                    end
+                    if (divisions==1)
+                        Pgen(2,i)=sPmin(i,k);
                     end
                 end
                 
@@ -101,27 +103,56 @@ for n=1:maxiter
                     TotalCost1;
                     Oldeps=eps1;
                     OldCostMin=TotalCost1;
+                    lamdaOld=lamda;
                 end
                 if(eps2==0)&&(TotalCost2<OldCostMin)
                     Ans=Pgen;
                     TotalCost2;
                     Oldeps=eps2;
                     OldCostMin=TotalCost2;
+                    lamdaOld=lamda;
                 end
                 
             end
         end
     end
-    lamda
-    Pgen
+    lamda;
+    Pgen;
     %plot(Pgen,lamda,'o')
-    Pgen=[0 0 0;0 0 0];
+    Pgen=[Pmin(1) Pmin(2) Pmin(3);Pmax(1) Pmax(2) Pmax(3)];
 
 end
 
-Pgen=Ans;
+
+Pgen=Ans(2,:);
+lamda=lamdaOld;
+
+%     hold on
+%     stairs([sPmin(1,1) sPmax(1,1)],[s(1,1),s(1,1)],'r')
+%     stairs([sPmin(1,2) sPmax(1,1)],[s(1,1),s(1,2)],'r')
+%     stairs([sPmin(1,2) sPmax(1,2)],[s(1,2),s(1,2)],'r')
+% 
+%     stairs([sPmin(2,1) sPmax(2,1)],[s(2,1),s(2,1)],'b')
+%     stairs([sPmin(2,2) sPmax(2,1)],[s(2,1),s(2,2)],'b')
+%     stairs([sPmin(2,2) sPmax(2,2)],[s(2,2),s(2,2)],'b')
+%     
+%     stairs([sPmin(3,1) sPmax(3,1)],[s(3,1),s(3,1)],'g')
+%     stairs([sPmin(3,2) sPmax(3,1)],[s(3,1),s(3,2)],'g')
+%     stairs([sPmin(3,2) sPmax(3,2)],[s(3,2),s(3,2)],'g')
+%     xlabel('Pgen (MW)');
+%     ylabel('dF/dPgen ($/MWh)');
+%     text([500],[9.5],'Unit 1');
+%     text([300],[9.15],'Unit 2');
+%     text([160],[9.6],'Unit 3');
+    %plot([50,600],[lamda,lamda])
+    %plot(Pgen,lamda,'o')
+
+%plot(Pgen(1,:),[s(1,1) lamdaOld s(3,1)],'o')
 TotalCost=0;
 for i=1:Ngen
     TotalCost=TotalCost+F(i,Pgen(1,i));
 end
+
+Pgen
 TotalCost
+lamda
