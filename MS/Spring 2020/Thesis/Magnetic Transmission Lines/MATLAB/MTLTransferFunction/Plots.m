4f8f0708d226905daff02efca1e356ce278147a6
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-06-21 02:46:27 +0500

    V1

---
+++ MS/Spring 2020/Thesis/Magnetic Transmission Lines/MATLAB/MTLTransferFunction/Plots.m
@@ -0,0 +1,57 @@
+hold on;
+% subplot(3,2,1)
+% plot(Vinput.time,Vinput.signals.values);
+% xlabel('Time (s)');
+% ylabel('Input Voltage (V)');
+% axis([0.24997 0.25 -200 200]);
+% 
+% subplot(3,2,2)
+% plot(Iprimary.time,Iprimary.signals.values);
+% xlabel('Time (s)');
+% ylabel('Input Current (A)');
+% axis([0.24995 0.25 0 125]);
+% 
+% 
+% subplot(3,2,3)
+% hold on;
+% plot(Vou.time,Vou.signals.values);
+% xlabel('Time (s)');
+% ylabel('Output Voltage 1 (V)');
+% axis([0.24997 0.25 4.88 4.89]);
+% 
+% subplot(3,2,4)
+% hold on;
+% plot(Iou.time,Iou.signals.values);
+% xlabel('Time (s)');
+% ylabel('Output Current 1 (A)');
+% axis([0.24997 0.25 97.6 97.8]);
+% 
+% 
+% subplot(3,2,5)
+% hold on;
+% plot(Vol.time,Vol.signals.values);
+% xlabel('Time (s)');
+% ylabel('Output Voltage 2 (V)');
+% axis([0.24997 0.25 14.92 14.94]);
+% 
+% subplot(3,2,6)
+% hold on;
+% plot(Iol.time,Iol.signals.values);
+% xlabel('Time (s)');
+% ylabel('Output Current 2 (A)');
+% axis([0.24997 0.25 14.92 14.94]);
+
+
+subplot(2,1,1)
+hold on;
+plot(PermeanceVm.time,PermeanceVm.signals.values);
+xlabel('Time (s)');
+ylabel('Permeance Voltage (A.t)');
+axis([0.2499 0.25 0 1.5e4]);
+
+subplot(2,1,2)
+hold on;
+plot(PermeanceIm.time,PermeanceIm.signals.values);
+xlabel('Time (s)');
+ylabel('Permeance Current (V)');
+axis([0.2499 0.25 -3 2]);
