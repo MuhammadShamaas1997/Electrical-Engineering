40344a7f110b03ca26c2f8c7136da23d415caf14
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-06-20 09:45:53 +0500

    V1

--- MS/Spring 2020/Thesis/Magnetic Transmission Lines/MATLAB/MTLTransferFunction/SaturationProfile.m
+++ MS/Spring 2020/Thesis/Magnetic Transmission Lines/MATLAB/MTLTransferFunction/SaturationProfile.m
@@ -1,37 +1,37 @@
-% clc;clear all;
-% max=20000;
-% MMF(1:max+1)=1:max+1;
-% K1=1;
-% K1exp=0.0002;
-% K2=2;
-% K2exp=0.0002;
-% 
-% ind=1;
-% 
-% for ind=1:max+1
-%         Ea(ind)=(150)*(1+exp(-K1exp*MMF(ind))*K1-exp(-K2exp*MMF(ind))*K2);
-% end
-% %Ea(ind)=150+exp(-0.005*u(1))*150-exp(-0.005*u(1))*300;
-% %Ea(ind)=u(1)*0.13-u(1)*u(1)*0.00003
+clc;clear all;
+max=2000;
+MMF(1:max+1)=-max/2:max/2;
+K1=1;
+K1exp=0.0002;
+K2=2;
+K2exp=0.0002;
+
+ind=1;
+
+for ind=1:max+1
+        Ea(ind)=(150)*(1+exp(-K1exp*MMF(ind))*K1-exp(-K2exp*MMF(ind))*K2);
+end
+%Ea(ind)=150+exp(-0.005*u(1))*150-exp(-0.005*u(1))*300;
+%Ea(ind)=u(1)*0.13-u(1)*u(1)*0.00003
 % plot(1:max+1,Ea)
 
 
-% for ind=1:max+1
-%         muap(ind)=10000*exp(-(abs(MMF(ind)-500))*(abs(MMF(ind)-500))/50000);
-%         muan(ind)=-10000*exp(-(abs(MMF(ind)+500))*(abs(MMF(ind)+500))/50000);
-% end
-% 
-% % hold on;
-% % plot((-max/2):1:(max/2),muap)
-% % plot((-max/2):1:(max/2),muan,'r')
-% % grid on;
-% % xlabel('H (A/m)');
-% % ylabel('Magnetic Susceptibility \chi_m');
-% % legend('\chi_m^+','\chi_m^-')
-% 
-% B(1)=2.5;
+for ind=1:max+1
+        muap(ind)=10000*exp(-(abs(MMF(ind)-250))*(abs(MMF(ind)-250))/50000);
+        muan(ind)=-10000*exp(-(abs(MMF(ind)+250))*(abs(MMF(ind)+250))/50000);
+end
+
+hold on;
+plot((-max/2):1:(max/2),muap)
+%plot((-max/2):1:(max/2),muan,'r')
+grid on;
+xlabel('H (A/m)');
+ylabel('Magnetic Susceptibility \chi_m');
+%legend('\chi_m^+','\chi_m^-')
+
+% B(1)=0;
 % Hmax=1000;
-% Happ=[Hmax:-1:(-Hmax) (-Hmax):1:(Hmax)];
+% Happ=[0:Hmax/4 Hmax:-1:(-Hmax) (-Hmax):1:(Hmax)];
 % hold on;
 % for ind=2:length(Happ)
 %     if (Happ(ind)>Happ(ind-1))
@@ -44,10 +44,10 @@
 %     B(ind)=B(ind-1)+dB;
 % end
 % 
-% % plot(Happ,B);
-% % grid('on');
-% % xlabel('H (A/m)');
-% % ylabel('B (T)');
+% plot(Happ,B);
+% grid('on');
+% xlabel('H (A/m)');
+% ylabel('B (T)');
 
 % subplot(2,3,1)
 % plot(ScopeData5.time,ScopeData5.signals(1,6).values)
@@ -69,9 +69,9 @@
 % xlabel('Time (s)');ylabel('Terminal Voltage (V)');axis([0 0.04 50 150]);
 
 % subplot(2,1,1)
-[mmf,emf]=dsort(ScopeData5.signals(1,3).values(200:2061),ScopeData5.signals(1,4).values(200:2061));
-plot(mmf,emf);
-ylabel('Generated EMF (V)');xlabel('Net MMF (rpm)');
+% [mmf,emf]=dsort(ScopeData5.signals(1,3).values(200:2061),ScopeData5.signals(1,4).values(200:2061));
+% plot(mmf,emf);
+% ylabel('Generated EMF (V)');xlabel('Net MMF (rpm)');
 % axis([400 2200 0 150]);
 % subplot(2,1,6)
 % plot(ScopeData5.time,ScopeData5.signals(1,5).values)
