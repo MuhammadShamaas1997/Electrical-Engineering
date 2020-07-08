c7db8c40196e30c6c133a2b37e6d10917f8623cd
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-07-09 00:09:07 +0500

    V1

---
+++ MS/Spring 2020/Smart Grids/HW/New folder/Chapter_6/Application_Problem/Read_Me.m
@@ -0,0 +1,11 @@
+% These folder contain solutions for the problems related to dianostic of PV cells in the book "Digital Twin Development and Deployment on the Cloud" Elsevier, 2020, Nassim Khaled, Bibin Pattel and Affan Siddiqui
+% 
+% Website for the book and other resources: https://www.practicalmpc.com/digital-twins
+% 
+% Chapter 6
+% Chapter_6/Application_Problem: Contains the diagnostic model of the PV. Note that in this model PV 9 has a failure 
+
+%Instructions: 
+%Load the .mat and then run Application_Problem_Solution.slx
+load model_data_application.mat
+
---
+++ MS/Spring 2020/Smart Grids/HW/New folder/Chapter_6/ReadMe.m
@@ -0,0 +1,12 @@
+% These four folders contain solutions for problems related to ball on plate for "Digital Twin Development and Deployment on the Cloud" Elsevier, 2020, Nassim Khaled, Bibin Pattel and Affan Siddiqui
+% 
+% Website for the book and other resources: https://www.practicalmpc.com/digital-twins
+% 
+% Chapter 6
+% Chapter_6/Solar_Cell_Modeling_of_the_PV_Subsystem: Contains the first model of the PV 
+% Chapter_6/Simulation_Results: Contains an improved version of the PV model with correction for ambient temperature
+% Chapter_6/Application_Problem: Contains the diagnostic model of the PV. Note that in this model PV 9 has a failure 
+
+
+% Instructions: 
+% Load the .mat in each folder and then run the Simulink models
