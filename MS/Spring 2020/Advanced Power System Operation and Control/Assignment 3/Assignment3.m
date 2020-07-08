a2c429171b2525f09aec64155036901d5bdbee2f
Muhammad Shamaas <mshamaas1997@gmail.com>
2020-07-08 14:34:09 +0500

    V1

--- MS/Spring 2020/Advanced Power System Operation and Control/Assignment 3/Assignment3.m
+++ MS/Spring 2020/Advanced Power System Operation and Control/Assignment 3/Assignment3.m
@@ -1,15 +1,7 @@
 clc;clear all;
 
-f2=ones(18,9).*inf;
-f2min=ones(18,1).*inf;
-P1min=zeros(18,1);
-P2min=zeros(18,1);
-D12=zeros(18,1);
-Ans12=zeros(18,1);
-
-f3=ones(27,9).*inf;
-f3min=ones(27,1).*inf;
-P3min=zeros(27,1);
+D12=zeros(27,1);
+Ans12=zeros(27,1);
 D123=zeros(27,1);
 Ans123=zeros(27,1);
 
@@ -24,48 +16,84 @@ Costs=[ inf    inf  inf;
         2760   inf  inf;
         inf    inf  inf];
 
-i1min=2;i2min=2;i3min=2;
-i1max=8;i2max=6;i3max=7;
-D12min=100;D12max=350;
-D123min=300;D123max=325;
+for r=1:9
+    for c=1:9
+        D12(r,1)=Powers(r,1);
+        D12(r+c,1)=max(D12(r+c,1),Powers(r)+Powers(c));
+        D12(r,2)=Costs(r,1);
+        D12(r+c,c+2)=Costs(r,1)+Costs(c,2);
+    end
+end
 
-for c=i2min:i2max
-    for r=i1min:i1max
-        sumP=Powers(r)+Powers(c);
-        if (sumP>=D12min)&&(sumP<=D12max)
-            f2(r+c,c)=Costs(r,1)+Costs(c,2);
-            if (f2(r+c,c)==min(f2(r+c,:)))&&(f2(r+c,c)~=inf)
-                f2min(r+c,1)=min(f2(r+c,:));
-                P2min(r+c,1)=Powers(c,1);
-                P1min(r+c,1)=Powers(r,1);
-                D12(r+c,1)=Powers(r)+Powers(c);
-                Ans12(r+c,1)=D12(r+c,1);
-                Ans12(r+c,2)=f2min(r+c,1);
-                Ans12(r+c,3)=P2min(r+c,1);
-                Ans12(r+c,4)=P1min(r+c,1);
+for r=1:18
+    for c=1:8
+        if (D12(r,c+1)==0)
+            D12(r,c+1)=inf;
+        end
+        if (D12(r,10)==inf)
+            D12(r,10)=0;
+        end
+    end
+end
+
+for r=1:18
+    for c=3:8
+        D12(r,9)=min(D12(r,3:8));
+        if (min(D12(r,3:8))~=inf)
+            if (D12(r,9)==D12(r,c))
+                D12(r,10)=Powers(c-2);
+            end
+            if (D12(r,9)==D12(r,c))
+                D12(r,11)=D12(r,1)-D12(r,10);
             end
         end
     end
 end
 
-for c=i3min:i3max
-    for r=1:length(f2min)
-        sumP=D12(r)+Powers(c);
-        if (sumP>=D123min)&&(sumP<=D123max)
-            f3(r+c,c)=f2min(r,1)+Costs(c,3);
-            if (f3(r+c,c)==min(f3(r+c,:)))&&(f3(r+c,c)~=inf)
-                f3min(r+c,1)=min(f3(r+c,:));
-                P3min(r+c,1)=Powers(c,1);
-                D123(r+c,1)=D12(r)+Powers(c,1);
-                Ans123(r+c,1)=D123(r+c,1);
-                Ans123(r+c,2)=f3min(r+c,1);
-                Ans123(r+c,3)=P3min(r+c,1);
-                Ans123(r+c,4)=P2min(r,1);
-                Ans123(r+c,5)=P1min(r,1);
+D12(1:14,:)
+Ans12=[D12(:,1) D12(:,9) D12(:,10) D12(:,11)];
+Ans12(1:15,1:3)
+
+for r=1:18
+    for c=1:9
+        D123(r,1)=Ans12(r,1);
+        D123(r+c,1)=max(D123(r+c,1),Ans12(r,1)+Powers(c));
+        D123(r,2)=Ans12(r,2);
+        D123(r+c,c+2)=Ans12(r,2)+Costs(c,3);
+    end
+end
+
+for r=1:27
+    for c=1:8
+        if (D123(r,c+1)==0)
+            D123(r,c+1)=inf;
+        end
+        if (D123(r,10)==inf)
+            D123(r,10)=0;
+        end
+    end
+end
+
+for r=1:27
+    for c=3:8
+        D123(r,10)=min(D123(r,3:9));
+        if (min(D123(r,3:9))~=inf)
+            if (D123(r,10)==D123(r,c))
+                D123(r,11)=Powers(c-2);
             end
         end
     end
 end
 
-Ans12
-Ans123
+for r123=1:27
+    for r12=1:18
+        if ((D123(r123,1)-D123(r123,11))==Ans12(r12,1))
+            D123(r123,12)=Ans12(r12,3);
+            D123(r123,13)=Ans12(r12,4);
+        end
+    end
+end
+
+D123(12:13,1:11)
+Ans123=[D123(:,1) D123(:,10) D123(:,11) D123(:,12) D123(:,13)];
+Ans123(12:13,:)
